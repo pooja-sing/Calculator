50f116c12366ebb1631e9b3e31b003044f2728f8
Pooja Singh <poojasingh31280@gmail.com>
2024-05-21 13:32:58 +0530

    changes

---
+++ src/Calculator.js
@@ -0,0 +1,41 @@
+
+import React, { useState } from 'react';
+import './Calculator.css';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input)); 
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <input type="text" value={input} readOnly />
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        {['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', 'C', '0', '=', '/'].map((button) => (
+          <button key={button} onClick={() => handleClick(button)}>
+            {button}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
